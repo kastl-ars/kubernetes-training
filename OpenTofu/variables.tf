172aa1a8e0e9fe2164959f87d6e93e412e108699
Johannes Kastl <johannes.kastl@ars.de>
2025-07-11 14:49:48 +0200

    ADD OpenTofu/variables.tf

---
+++ OpenTofu/variables.tf
@@ -0,0 +1,15 @@
+variable "host" {
+  type = string
+}
+
+variable "client_token" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "ingress_hostname" {
+  type = string
+}
